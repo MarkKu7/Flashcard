0f585c90943013f458ec77a88d4c5ef456d4786d
MarkKu7 <marksrc7@gmail.com>
2021-08-10 13:48:09 -0700

    added variable definition

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
-    $("p").click(function() {
-        $(".flashcard-showing").toggle();
-        $(".flashcard-hidden").toggle();
+    $("#operator").click(function() {
+        $(".flashcard-showing#operators").toggle();
+        $(".flashcard-hidden#operatorh").toggle();
+    });
+    $("#javascript").click(function() {
+      $(".flashcard-showing#javascripts").toggle();
+      $(".flashcard-hidden#javascripth").toggle();
+     });
+    $("#variable").click(function() {
+      $(".flashcard-showing#variables").toggle();
+      $(".flashcard-hidden#variableh").toggle();
     });
 });
